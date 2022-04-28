f599e30c552c253d723d1eafd4a1e5af16523027
jestats <jestats2018@gmail.com>
2022-04-28 16:08:34 -0700

    first commit

---
+++ src/js.js
@@ -0,0 +1,55 @@
+function search(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#search-text-input");
+  searchCity(cityInput.value);
+}
+function searchCity(city) {
+  let apiKey = "2a2eb7984c02aca7e1add2c64025b4ae";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  console.log(temperature);
+  console.log(response);
+  let name = response.data.name;
+  let cityName = `${name}`;
+  let cityTemp = `${temperature}`;
+  let city = document.querySelector(".location");
+  let currentTemp = document.querySelector(".lower-text");
+  city.innerHTML = cityName;
+  currentTemp.innerHTML = `${cityTemp} °C`;
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+let apiKey = "2a2eb7984c02aca7e1add2c64025b4ae";
+let units = "metric";
+let city = "Flagstaff, Arizona";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(showTemperature);
+
+let now = new Date();
+let time = document.querySelector(".dayTime");
+
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let day = days[now.getDay()];
+
+time.innerHTML = `${day} ${date}, ${hours}:${minutes}, ${year}`;
